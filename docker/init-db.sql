f06879a9ea7e946757257689714dd5e1a7d6c326
Sören Wacker <s.wacker@tudelft.nl>
2026-06-22 21:00:44 +0200

    provision the pytest database on first postgres startup so a fresh setup can run the suite

--- docker/init-db.sql
+++ docker/init-db.sql
@@ -4,3 +4,7 @@
 -- Create keycloak database if it doesn't exist
 SELECT 'CREATE DATABASE keycloak'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')\gexec
+
+-- Create the test database used by the pytest suite if it doesn't exist
+SELECT 'CREATE DATABASE metaseed_hub_test'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'metaseed_hub_test')\gexec
